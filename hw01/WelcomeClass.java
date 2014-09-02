3295b7f6b10a27aa61cd58df3260248126126a44
Deep Sheth <djsdeep@gmail.com>
2014-09-02 00:50:49 +0000

    Creates WelcomeClass in correct directory

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+// Deep Sheth
+// Welcome Class Program
+
+//WelcomeClass is created and main is declared
+public class WelcomeClass {
+    public static void main(String[] args){
+        
+        //Output decorative welcome message
+        System.out.println("   -----------");
+        System.out.println("   | WELCOME |");
+        System.out.println("   -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-D--S--S--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        
+        //Autobiography Tweet
+        
+        System.out.println("I came to America when I was three and I only visted my hometown once since then. I'm from India!");
+    
+    }
+}
