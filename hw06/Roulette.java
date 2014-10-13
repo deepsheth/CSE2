ddde0536f16ccdf470c78897427935a31f4085af
Deep Sheth <djsdeep@gmail.com>
2014-10-13 02:21:12 +0000

    Major fixes: 1000 repetitions are of 100 spin outcomes, output reflects 1000 repetitions

--- hw06/Roulette.java
+++ hw06/Roulette.java
@@ -2,7 +2,7 @@
 // dss218
 // Roulette
 // October 14, 2014
-// Program stimulates outcomes of player's 100 spins based on Monte Carlo simulatiosn
+// Monte Carlo simulation: Program stimulates outcomes of 100 spins of roulette when repeated 1000 times 
 
 //Method
 import java.util.Scanner;
@@ -13,16 +13,20 @@ public class Roulette {
         Scanner input = new Scanner(System.in);
         
         //Variables
-        String playerInput = ""; //player input as string
-        int playerNum = 0; //player input as int
-        boolean num00 = false; //if player picks 00
-        int win$ = 36; //money won per match
-        int nWins = 0; //times won
-        int nLosses = 0; //times lost
+        int nWins = 0; //spins won 
+        int nLosses = 0; //spins lost
+        int gamesWon = 0; //games won
+        int gamesWonSome = 0; //games partially won
+        int gamesLost = 0; //games lost
         
  /*     ============================================================
            .::[ CODE TO ALLOW USER TO CHOOSE NUMBER TO BET ON ]::.
         =============================================================
+        
+        //Variables 
+        String playerInput = ""; //player input as string
+        int playerNum = 0; //player input as int
+        boolean num00 = false; //if player picks 00
         
         //Asks user for input
         System.out.print("What number would you like to bet on? ");
@@ -58,50 +62,46 @@ public class Roulette {
        System.out.println(" * * * * R O U L E T T E  * * * * ");
        
        //Program picks user's number
-       playerNum = (int)(Math.random()*38);
-       System.out.println("The number you are betting on is " + playerNum);
-       
-       for (int i=0; i<100; i++) {
-            int rouletteNum = (int)(Math.random()*38); //Generates random number from 0-37
-            
-            if (rouletteNum == playerNum) //case for win
-               nWins++;
-            else  //case for loss
-               nLosses++;
-            
-       }
-       
-       //Outputs outcome of spins, including total money won
-       System.out.println("\nIf you spin 100 times, here are your outcomes:");
-       int totalWins$ = win$*nWins;
-       System.out.println("You would win "+nWins+" times.\n\tYou win $"+win$+" * "+nWins+" = $"+totalWins$);
-       System.out.println("You would lose "+nLosses+" times.");
-       
-       //Determines if net money results profit/loss
-       if ((totalWins$ - 100) >= 0)
-            System.out.println("Congragulations! You leave winning $" + (totalWins$ - 100));
-       else 
-            System.out.println("\nSorry. You leave losing $" + (100 - totalWins$));
+       int playerNum = (int)(Math.random()*38);
+       System.out.println("The number you are betting on is " + playerNum + ".");
        
        
-       //Resets counters for 1000000 theoretical case
-       nWins=0;
-       nLosses=0;
-       
-       for( int i=0; i<100000; i++) {
-           int rouletteNum = (int)(Math.random()*38); //Generates random number from (0,38) & truncates decimals
+       for (int n=0; n<1000; n++) { //100 spins are repeated 1000 times
            
-           if (rouletteNum == playerNum) 
-               nWins++;
-           else 
-               nLosses++;
-               
-        
-       } 
-        
-        //SPIN 1000x
-        System.out.println("\n\nWins: " + nWins);
-        System.out.println("Losses: " + nLosses);
+           for (int i=0; i<100; i++) { //player spins 100 times
+                int rouletteNum = (int)(Math.random()*38); //Generates random number from 0-37
+                
+                //counts number of times player wins/loses per 100 spisn
+                if (rouletteNum == playerNum) //case for win
+                   nWins++;
+                else  //case for loss
+                   nLosses++;
+           }//end for
+           
+            //3 wins means player profited 
+            if (nWins >= 3) 
+               gamesWon++;
+            //0 wins means player losses everything
+            else if (nWins == 0) 
+               gamesLost++;
+            //1 or 2 wins means player wins some money back
+            else {
+               gamesWon++;
+               gamesWonSome++;
+            }
+           
+            //resets counters for next game
+            nWins=0;
+            nLosses=0;
+       
+        } //end for loop simulations
+       
+        //Outputs outcome of 100 spins played 1000 times
+        System.out.println("\n1000 repetitions of 100 spin simulation:");
+        System.out.println("\tNumber of games you win a profit: " + gamesWon);
+        System.out.println("\tNumber of games you win some money back " + gamesWonSome);
+        System.out.println("\tNumber of games you lose everything: " + gamesLost);
+       
 
-    }
-}
+    } //end main
+} //end class
