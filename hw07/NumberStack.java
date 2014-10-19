d950234f749325ddee43344f144bd32ad8d3ec09
Deep Sheth <djsdeep@gmail.com>
2014-10-19 00:54:29 +0000

    For loop FULLY functional with leading spaces now.

--- hw07/NumberStack.java
+++ hw07/NumberStack.java
@@ -28,31 +28,48 @@ public class NumberStack {
             userNum = input.nextInt();
             
         if (userNum < 1 || userNum > 9) {
-            System.out.println("You did not enter an integer between 1 -9.");
+            System.out.println("You did not enter an integer between 1 - 9.");
             return; 
         }
+            
+        userNum2 = userNum; //creates copy of variable userNum
+        String space = "";  //variable initialized - will be used for adding spaces before numbers
         
-       // numLength=(userNum*2)-1;
-        int numLengthReset = 1;
+        System.out.print("USING A FOR LOOP:");
         
+        //PRINTS NUMBERS FROM 1 -> WHAT USER INPUTTED
         for (int a=0; a<=userNum; a++) { //Creates a new number
-            numLength = (a*2)-1;
+            numLength = (a*2)-1;         //How many digits will be printed of the number
+  
+            //PRINTS LINES OF THE NUMBER
+            for(int b=0; b<a; b++) {           //number of lines of the number is outputted
             
-            for(int d=0; d<a; d++) {
-                for(int b=0; b<numLength; b++) { //number of times the number is outputted
-                    
-                    System.out.print(a);
+                space = "%"+(userNum2+1)+"s";  //number of spaces to add before each line
+                System.out.printf(space, "");  //prints spaces before new line
+                
+                //PRINTS INDIVIDUAL NUMBER
+                for(int c=0; c<numLength; c++) { //number of times the number is outputted
+                    System.out.print(a);         //Prints out the individual number
                 }
-                System.out.println(); // New line follows the number outputtted
-            }    
+                System.out.println();            //New line follows the line of the number outputtted
+            }
             
-            for(int c=0; c<numLength; c++) { //number of times dashes will be displayed after numbers are outputted
+            //PRINTS DASHES
+            System.out.printf(space, "");    //Adds proper spacing before dashes
+            for(int b=0; b<numLength; b++) { //Number of times dashes will be displayed after numbers are outputted
                 System.out.print("-");
             }
+            
             System.out.println(); // New line follows dashes
-            
-            
-        }
+            userNum2--;           // Decreases space before new lines
+        } //END OUT FOOR LOOP
         
+        
+        
+        int a=0, b=0, c=0;
+        System.out.println("USING A WHILE LOOP");
+        
+
+
     }
 }
