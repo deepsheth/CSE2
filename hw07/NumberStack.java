6251c146276c2a642c26943a1877d546b79773b5
Deep Sheth <djsdeep@gmail.com>
2014-10-19 02:32:44 +0000

    Program is fully complete and functional!

--- hw07/NumberStack.java
+++ hw07/NumberStack.java
@@ -35,6 +35,10 @@ public class NumberStack {
         userNum2 = userNum; //creates copy of variable userNum
         String space = "";  //variable initialized - will be used for adding spaces before numbers
         
+        /////////////////////////////////////
+        ///// USING FOR LOOP
+        /////////////////////////////////////
+        
         System.out.print("USING A FOR LOOP:");
         
         //PRINTS NUMBERS FROM 1 -> WHAT USER INPUTTED
@@ -48,28 +52,93 @@ public class NumberStack {
                 System.out.printf(space, "");  //prints spaces before new line
                 
                 //PRINTS INDIVIDUAL NUMBER
-                for(int c=0; c<numLength; c++) { //number of times the number is outputted
+                for(int c=0; c<numLength; c++)   //number of times the number is outputted
                     System.out.print(a);         //Prints out the individual number
-                }
                 System.out.println();            //New line follows the line of the number outputtted
             }
             
             //PRINTS DASHES
             System.out.printf(space, "");    //Adds proper spacing before dashes
-            for(int b=0; b<numLength; b++) { //Number of times dashes will be displayed after numbers are outputted
+            for(int b=0; b<numLength; b++)   //Number of times dashes will be displayed after numbers are outputted
                 System.out.print("-");
-            }
-            
+                
             System.out.println(); // New line follows dashes
             userNum2--;           // Decreases space before new lines
         } //END OUT FOOR LOOP
         
+        /////////////////////////////////////
+        ///// USING WHILE LOOP
+        /////////////////////////////////////
         
+        System.out.print("USING A WHILE LOOP");
+        int a=0, b, c;
+        userNum2 = userNum; //Resets value of userNum2
         
-        int a=0, b=0, c=0;
-        System.out.println("USING A WHILE LOOP");
+        while (a<=userNum) {
+            numLength = (a*2)-1;         //How many digits will be printed of the number
+            b=0;
+            
+            while (b<a) {
+                space = "%"+(userNum2+1)+"s";  //number of spaces to add before each line
+                System.out.printf(space, "");  //prints spaces before new line
+                c=0;
+                
+                while (c<numLength) {
+                    System.out.print(a);
+                    c++;
+                }
+                System.out.println();
+                b++;    
+            }
+            System.out.printf(space, "");
+            b=0;
+            
+            while (b<numLength) {
+                System.out.print("-");
+                b++;
+            }
+            System.out.println();
+            userNum2--;
+            a++;
+        }
         
-
-
+        /////////////////////////////////////
+        ///// USING DO-WHILE LOOP
+        /////////////////////////////////////
+        
+        System.out.print("USING A DO-WHILE LOOP");
+        userNum2 = userNum; //Resets value of userNum2
+        
+        a=0;
+        do{ 
+            
+            numLength = (a*2)-1;         //How many digits will be printed of the number
+            b=0;
+            do{
+                
+                space = "%"+(userNum2+1)+"s";  //number of spaces to add before each line
+                System.out.printf(space, "");  //prints spaces before new line
+                c=0;
+                do {
+                    if (a==0) break; //Prevents 0 from outputting before 1
+                    System.out.print(a);
+                    c++;
+                } while(c<numLength);
+                System.out.println();
+                b++; 
+            } while(b<a);
+            if (a!=0) System.out.printf(space, ""); //Prevents extra spacing on first line
+            
+            b=-1;
+            do { 
+                if (b!=-1) 
+                    System.out.print("-");
+                b++;
+            } while(b<numLength);    
+            
+            if (a!=0) System.out.println(); //Prevents extra new line after title
+            userNum2--;
+            a++;
+        } while (a<=userNum);
     }
 }
