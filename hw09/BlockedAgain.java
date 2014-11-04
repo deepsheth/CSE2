3c9b5e5080a8db18fe5e09189ca1f9c5939ca257
Deep Sheth <djsdeep@gmail.com>
2014-11-04 23:19:09 +0000

    Adds proper documentation

--- hw09/BlockedAgain.java
+++ hw09/BlockedAgain.java
@@ -8,6 +8,7 @@
 import java.util.Scanner;
 
 public class BlockedAgain{
+    //MAIN METHOD (GIVEN)
     public static void main(String []s){
         int m;
         
@@ -30,70 +31,67 @@ public class BlockedAgain{
     //Forces user to enter integer
     public static int checkInt(){
         Scanner s = new Scanner(System.in);
-        //If user input is not an int., loop will run until user enters int
+        //Loop will run until user enters an int
+        
         while (!s.hasNextInt()){
             System.out.print("You must enter an integer. Try again: ");
-            s.next();
+            s.next(); //Prevents infinite loop, asks user to retry
         } 
+        
         //Returns integer value
         int input = s.nextInt();
         return input;
-        
     }
     
     
     //Forces user to enter a number in the range [1,9]
     public static int checkRange(int userInt) {
-        
-        //If user input is not within range, user will be forced to
         while (userInt<1 || userInt>9){
             System.out.print("Enter a number in the range [1,9]. Try again: ");
             userInt = checkInt();
         }
-        return userInt; //returns if in range
+        return userInt; //returns integer in specified range
     }
     
-    
+    //Calls block method
     public static void allBlocks(int userNum) {
-        int userNum2 = userNum;
+        int userNum2 = userNum; //Creates copy of variable
         String space = " ";
         block(userNum, userNum2, space);
     }
     
     
+    //Prints number from 1 -> user input
     public static void block(int userNum, int userNum2, String space){
         for (int a=0; a<=userNum; a++) { //Creates a new number
-            userNum2--;
+            userNum2--; // Decreases space before new lines
             line(a, userNum, userNum2, space);
         }
     }
     
     
+    //Creates line and dashes
     public static void line(int a, int userNum, int userNum2, String space){
         int numLength = (a*2)-1;         //How many digits will be printed of the number
         
-       
         //PRINTS LINES OF THE NUMBER
         for(int b=0; b<a; b++) {           //number of lines of the number is outputted
             int c=0;
             
             space = "%"+(userNum2+2)+"s";  //number of spaces to add before each line
             System.out.printf(space, "");  //prints spaces before new line
-            
             individualNums(a,numLength);
         }
         
+        //Dashes
         System.out.printf(space, "");    //Adds proper spacing before dashes
         for(int b=0; b<numLength; b++)   //Number of times dashes will be displayed after numbers are outputted
         System.out.print("-");
-       
-        
         System.out.println(); // New line follows dashes
-        userNum2--;           // Decreases space before new lines
     }
     
+    //PRINTS INDIVIDUAL NUMBER
     public static void individualNums(int a, int numLength){
-        //PRINTS INDIVIDUAL NUMBER
         for(int c=0; c<numLength; c++)   //number of times the number is outputted
             System.out.print(a);         //Prints out the individual number
         System.out.println();            //New line follows the line of the number outputtted
