0862dd0b9818bd4b8a1817063fbd7efaf262b7f9
Deep Sheth <djsdeep@gmail.com>
2014-11-15 03:23:13 +0000

    Allows program to restart

--- lab07-LoopTheLoop/LoopTheLoop.java
+++ lab07-LoopTheLoop/LoopTheLoop.java
@@ -11,27 +11,36 @@ public class LoopTheLoop {
         Scanner input  = new Scanner(System.in);
         int nStars = 0;
         
-        //Asks for user input
-        System.out.print("Enter the number of starts you want to print: ");
-        nStars = input.nextInt();
-        
-        if (nStars>15) {
-            System.out.println("Please enter a number no higher than 15."); return;
-        }
-        
-        //Outputs same number of stars as user's response
-        for (int i=0; i<nStars; i++)
-            System.out.print("*");
-        System.out.println("");
-        
-        
-        //j is one value less than i to allow linebreaks after stars have been outputted
-        for (int i=1;i<=nStars;i++) {     
-            for (int j=0; j<i; j++) 
+        while (true) {
+            //Asks for user input
+            System.out.print("Enter the number of stars you want to print [1-15]: ");
+            nStars = input.nextInt();
+            
+            if (nStars>15 || nStars<1) {
+                System.out.println("You number is not in the range [1-15]"); 
+                continue;
+            }
+            
+            //Outputs same number of stars as user's response
+            for (int i=0; i<nStars; i++)
                 System.out.print("*");
             System.out.println("");
-        }
-        
+            
+            
+            //j is one value less than i to allow linebreaks after stars have been outputted
+            for (int i=1;i<=nStars;i++) {     
+                for (int j=0; j<i; j++) 
+                    System.out.print("*");
+                System.out.println("");
+            }
+            
+            //Restarts program if user enters Y/y
+            System.out.print("Enter Y or y to restart: ");
+            String response = input.next();
+            if(!response.equals("Y") && !response.equals("y"))
+                break;
+            
+        }//end while true
 
     } //end main
 } //end class
