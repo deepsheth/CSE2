679fce47e8d6a23399e27fe9d25e7fe826c5284e
Deep Sheth <djsdeep@gmail.com>
2014-11-19 03:58:47 +0000

    done

--- hw10/PokerOdds.java
+++ hw10/PokerOdds.java
@@ -2,30 +2,33 @@
 // dss218
 // Find Duplicates
 // Nov. 18, 2014
-// Program generates poker hand from deck of cards and measures probability
+// Program generates poker hand from deck of cards and measures probability of picking a hand with exactly one duplicate
 
 import java.util.Random;
 import java.util.Scanner;
+
 public class PokerOdds{
-    int test = 0;
     public static void main(String [] arg){
         showHands();
         simulateOdds();
     }
 
     public static void showHands(){
+        
+        //Variables
         String answer = "";
         Scanner input=new Scanner(System.in);
         int[] hand = {-1, -1, -1, -1, -1};
         
         do {
-            
+            //Variables inside loop
             int[] deck = new int[52];
             String[] suitOut = {"Clubs: ", "Diamonds: ", "Hearts: ", "Spades: "};
             String cardNum = "";
             String suitType = "";
             String space = " ";
             
+            //Initializes all hand array elements to -1
             for(int i=0; i<5; i++)
                 hand[i] = -1;
                 
@@ -35,19 +38,23 @@ public class PokerOdds{
         
         
             for (int i=0; i<5; i++){    
+                //Num of deck cards generated decreases by 1 each run
                 int card = (int)(Math.random() * (52-i));
                 
-                hand[i] = deck[card];
+                
+                hand[i] = deck[card]; //Swaps card in hand with card in deck
                 deck[card] = deck[51-i];
-                deck[51-i] = -1;
+                deck[51-i] = -1; //Last number in deck becomes -1
+                
+                //cardRandom is card picked in hand
                 int cardRandom = hand[i];
                 
                 
-                cardNum = cardNum(cardRandom);
-                suitType = suitType(cardRandom);
-                // System.out.println("Random number: "+cardRandom);
-                // System.out.println("Suit: "+suitType+"   Number: "+cardNum);
+                cardNum = cardNum(cardRandom);   //Returns number of a card
+                suitType = suitType(cardRandom); //Returns type of suit
+
                 
+                //Output array stores suits and card numbers
                 switch(suitType) {
                     case "Clubs":
                         suitOut[0] += cardNum + space;
@@ -64,24 +71,21 @@ public class PokerOdds{
                 }
             } // end iteration
             
-            
+            //Outputs suits & cards picked from suit
             for(int i=0; i<suitOut.length; i++)
                 System.out.println(suitOut[i]);
-                
-            
-        
-        
-        
+
             //If user enters "Y/y", program will continue running using DO/WHILE loop
             System.out.print("Go again? Enter 'y' or 'Y', anything else to quit- ");
             answer=input.next();
-        } while (answer.equals("Y") || answer.equals("y"));    
-        
-        
+        } while (answer.equals("Y") || answer.equals("y"));
     }// end main
     
+    
+    //Method returns number/letter of card
     public static String cardNum(int cardRandom) {
         String cardNum;
+        
         switch(cardRandom%13){
             case 0: cardNum = "A";
                 break;
@@ -98,10 +102,12 @@ public class PokerOdds{
         return cardNum;
     }
     
+    //Method returns type of suit of random card picked
     public static String suitType(int cardRandom) {
-        int suit = (int)(cardRandom/13 + 0.5);
         String suitType = "";
+        int suit = (int)(cardRandom/13 + 0.5); 
         
+        //Depending on calculation above, card type is determined
         switch (suit) {
             case 0: suitType = "Clubs";
                 break;
@@ -115,96 +121,81 @@ public class PokerOdds{
         return suitType;
     }
     
+    //
     public static void simulateOdds() {
+        //Variables
         String answer = "";
-        int[] hand = {-1, -1, -1, -1, -1};
+        int[] hand = {-1, -1, -1, -1, -1}; 
         String[] duplicateOut = {"A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
         int[] numOfDuplicates = new int[13];
         
-    for(int z = 0; z<10000; z++){    
-        int[] deck = new int[52];
-        String[] suitOut = {"Clubs: ", "Diamonds: ", "Hearts: ", "Spades: "};
-        String cardNum = "";
-        String suitType = "";
-        String space = " ";
-        Boolean duplicate;
-        int cardRandom;
-        
-        
-            
-        for(int i=0; i<5; i++)
-            hand[i] = -1;
-            
-        //Gives deck of cards values
-        for(int i=0; i<deck.length; i++)
-            deck[i] = i;
-    
-    
-        for (int i=0; i<5; i++){    
-            int card = (int)(Math.random() * (52-i));
-            
-            hand[i] = deck[card];
-            deck[card] = deck[51-i];
-            deck[51-i] = -1;
-            cardRandom = hand[i];
+        for(int z = 0; z<10000; z++){   
+            //Variables
+            int[] deck = new int[52];
+            String[] suitOut = {"Clubs: ", "Diamonds: ", "Hearts: ", "Spades: "};
+            String cardNum = "";
+            String suitType = "";
+            Boolean duplicate;
+            int cardRandom;
             
             
-            cardNum = cardNum(cardRandom);
-            //System.out.println("Card Number: "+cardNum);
-            
-            
-            
-        }
-        
-        for(int i=0; i<5; i++){
-            hand[i] = hand[i]%13;
-           // System.out.println("Hand divided: "+hand[i]);
-        }
-        
-        
-        
-       //duplicate = FindDuplicates.exactlyOneDup(hand);
-       
-       
-       int numDuplicates = 0;
-        int totalDups = 0;
-        int cardWithDuplicate = -1;
-       
-        //Loop provides number to look for in array
-        for(int i=0; i<hand.length; i++){
-            int lookFor = hand[i];
-            
-            //Increases counter everytime numbers match
-            for(int n=0; n<hand.length; n++){
-                if (lookFor == hand[n])
-                    numDuplicates++;
-                    
-            }
-            
-            //Two matches are found -- only one duplicate may exist
-            if(numDuplicates == 2){
-                totalDups++; //New counter is increased (necessary in case more than two instances of same number)
-                cardWithDuplicate = lookFor;
+            //All hand cards are initialized to -1    
+            for(int i=0; i<5; i++)
+                hand[i] = -1;
                 
+            //Gives deck of cards values
+            for(int i=0; i<deck.length; i++)
+                deck[i] = i;
+        
+        
+            for (int i=0; i<5; i++){    
+                int card = (int)(Math.random() * (52-i));
+                
+                //Swaps cards
+                hand[i] = deck[card];
+                deck[card] = deck[51-i];
+                deck[51-i] = -1;
+                cardRandom = hand[i];
+                cardNum = cardNum(cardRandom);
             }
-            else if(numDuplicates >= 3) //Ends loop if more than 3 duplicates found
-                duplicate = false;
-            numDuplicates = 0; //Resets counter for next array search
-        }
-        
-        
-        if (totalDups == 2)
-            duplicate = true; //If pair exists, returns true
-        else {
-            duplicate = false;
-        }    
-       //System.out.println("Exactly 1 duplicate: "+duplicate);    
-       //System.out.println("Card With Duplicate: "+cardWithDuplicate);
-       
-       if (cardWithDuplicate != -1) {
-            numOfDuplicates[12 - cardWithDuplicate] += 1;
-       }
-    }//end thousand loop
+            
+            //Hand becomes a number from 0-12
+            for(int i=0; i<5; i++)
+                hand[i] = hand[i]%13;
+            
+            //Counters
+            int numDuplicates = 0;
+            int totalDups = 0;
+            int cardWithDuplicate = -1;
+           
+            //Loop provides number to look for in array
+            for(int i=0; i<hand.length; i++){
+                int lookFor = hand[i];
+                
+                //Increases counter everytime numbers match
+                for(int n=0; n<hand.length; n++){
+                    if (lookFor == hand[n])
+                        numDuplicates++;
+                        
+                }
+                
+                //Two matches are found -- only one duplicate may exist
+                if(numDuplicates == 2){
+                    totalDups++; //New counter is increased (necessary in case more than two instances of same number)
+                    cardWithDuplicate = lookFor;
+                    
+                }
+                else if(numDuplicates >= 3) //Ends loop if more than 3 duplicates found
+                    duplicate = false;
+                numDuplicates = 0; //Resets counter for next array search
+            }
+           //System.out.println("Exactly 1 duplicate: "+duplicate);    
+           //System.out.println("Card With Duplicate: "+cardWithDuplicate);
+           
+           if (cardWithDuplicate != -1) {
+                numOfDuplicates[12 - cardWithDuplicate] += 1;
+           }
+        }//end thousand loop
        
        System.out.println("Rank\tExactly One Duplicate");
        for(int i=0; i<13; i++){
