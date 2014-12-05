a7269ebcf7b12cb745802380f662e2a68e72ea17
Deep Sheth <djsdeep@gmail.com>
2014-12-05 01:46:17 +0000

    Majority of Program is complete.

--- hw11/PokerHands.java
+++ hw11/PokerHands.java
@@ -18,7 +18,6 @@ public class PokerHands {
         for(int i=0; i<hand.length; i++){
             boolean store = true;
             card = cardValidator(suitChoices, cardChoices, hand);
-            System.out.println(card);
             
             for(int j=0; j<hand.length; j++){
                 if (card.equals(hand[j])){
@@ -33,6 +32,7 @@ public class PokerHands {
         }
 
         output(suitOut, hand);
+        rank(hand);
     }
     
     public static String cardValidator(String[] suitChoices, String[] cardChoices, String[] hand) {
@@ -63,8 +63,10 @@ public class PokerHands {
                 num = input.next().toUpperCase(); // Input is not case-sensitive
                 
                 //Program will interpret card 10 as number 0
-                if(num.equals("0"))
+                if(num.equals("0")) {
+                    System.out.println("** You did not enter a vaild card number/letter. Try again. **");
                     continue;
+                }
                 else if (num.equals("10"))
                     num = "0";
                     
@@ -89,33 +91,113 @@ public class PokerHands {
             char suit = hand[i].charAt(0);
             char num = hand[i].charAt(1);
             
+            //Finds & stores all club cards to output variable
             for(int j=0; j<hand.length && i==0; j++){
                 if (hand[j].charAt(0) == 'C'){
-                    suitOut[0] += hand[j].charAt(1) + " ";
+                    if (hand[j].charAt(1) == '0')    
+                        suitOut[i] += "10" + " ";
+                    else
+                        suitOut[i] += hand[j].charAt(1) + " ";
                 }
             }
             
+            //Finds & stores all diamonds cards to output variable
             for(int j=0; j<hand.length && i==1; j++){
-                if (hand[j].charAt(0) == 'D'){
-                    suitOut[1] += hand[j].charAt(1) + " ";
-                }
+                if (hand[j].charAt(0) == 'D')
+                    suitOut[i] += hand[j].charAt(1) + " ";
             }
             
+            //Finds & stores all hearts cards to output variable
             for(int j=0; j<hand.length && i==2; j++){
-                if (hand[j].charAt(0) == 'H'){
-                    suitOut[2] += hand[j].charAt(1) + " ";
-                }
+                if (hand[j].charAt(0) == 'H')
+                    suitOut[i] += hand[j].charAt(1) + " ";
             }
             
+            //Finds & stores all club cards to output variable
             for(int j=0; j<hand.length && i==3; j++){
-                if (hand[j].charAt(0) == 'S'){
-                    suitOut[3] += hand[j].charAt(1) + " ";
+                if (hand[j].charAt(0) == 'S')
+                    suitOut[i] += hand[j].charAt(1) + " ";
+            }
+        }
+        
+        //Outputs hand
+        for(int i=0; i<suitOut.length; i++) {
+            System.out.println(suitOut[i]);
+        }
+    }
+    
+    public static void rank(String[] hand) {
+        int[] handNum = new int[5];
+        
+        for(int i=0; i<hand.length; i++){
+            switch(hand[i].charAt(1)){
+                case 'A': handNum[i] = 14; break;
+                case 'K': handNum[i] = 13; break;
+                case 'Q': handNum[i] = 12; break;
+                case 'J': handNum[i] = 11; break;
+                case '0': handNum[i] = 10; break;
+                default: handNum[i] = Character.getNumericValue(hand[i].charAt(1)); break;
+            }
+        }
+        
+        // SELECTION SORT on hand numbers
+        for (int a=0; a<handNum.length-1; a++) {
+            int minimum = handNum[a];
+            for (int b=a+1; b<handNum.length; b++){
+                if (handNum[b] < minimum) {
+                    minimum = handNum[b];
+                    handNum[b] = handNum[a];
+                    handNum[a] = minimum;
+                    
+                    
                 }
             }
         }
         
-        for(int i=0; i<suitOut.length; i++) {
-            System.out.println(suitOut[i]);
+        // Requirements for ROYAL/STRAIGHT FLUSH
+        boolean consecutive = false;
+        boolean sameSuit = false;
+        boolean consecutive3 = false;
+        boolean consecutive4 = false;
+        
+        // Checks if hand has consecutive numbers
+        for(int i=0, j=0; i<handNum.length-1; i++) {
+            if (handNum[i] == handNum[i+1]-1)
+                j++; // Counts number of consecutive numbers
+            if (j==3)
+                consecutive4 = true;
+            else if (j==4){
+                consecutive = true;
+                // If card numbers are consecutive, checks if suits are all same
+                for(int k=0, l=0; k<hand.length-1; k++) {
+                    if (hand[k].charAt(0) == hand[k+1].charAt(0))
+                        l++; // Counts number of consecutive suits
+                    if (l==4)
+                        sameSuit = true;
+                }
+            }
         }
+        
+        // When suits match and numbers are consecutive 
+        if (consecutive && sameSuit) {
+            // Outputs type of FLUSH
+            for (int i=0; i<handNum.length; i++) {
+                if (handNum[i] == 10-i) {
+                    System.out.println("This is a royal flush.");
+                    return;
+                }
+                else {
+                    System.out.println("This is a straight flush.");
+                    return;
+                }
+            }
+        }
+        
+        // FOUR OF A KIND Test
+        if (consecutive4) {
+            System.out.println("You have Four of a Kind.");
+            return;
+        }
+        
     }
 }
